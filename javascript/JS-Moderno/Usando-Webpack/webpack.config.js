90d00b41e4465eb67f0cdf28b3f6ecb6fa66bc3a
Filipe Campos <116318582+wantedxp@users.noreply.github.com>
2023-03-05 04:37:11 -0300

    finalizando exercicios webpack

---
+++ javascript/JS-Moderno/Usando-Webpack/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        hello: './src/hello.js'
+
+    },
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].bundle.min.js'
+    }
+}
