90f69e0b10bba82850044be94984b7979213e439
Filipe Campos <116318582+wantedxp@users.noreply.github.com>
2023-06-08 22:49:31 -0300

    aula compartilhamento de estados entre components

--- react/12-resolucao-exercicio-d/exercice-resolution/src/App.jsx
+++ react/12-resolucao-exercicio-d/exercice-resolution/src/App.jsx
@@ -1,6 +1,44 @@
-function App() {
-  
+import { useState } from "react";
+import "./index.css";
+import NumberInput from "./components/NumberInput";
+
+export default function App() {
+  const [password, setPassword] = useState("");
+  const [copy, setCopy] = useState("Copiar");
+  const [passwordSize, setPasswordSize] = useState(8);
+
+  function generate() {
+    const characters =
+      "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
+    let newPassword = "";
+
+    for (let i = 0; i < passwordSize; i++) {
+      newPassword += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    setPassword(newPassword);
+    setCopy("Copiar");
+  }
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(password);
+    setCopy("Copiado");
+  }
+
   return (
-    )
+    <div className="App">
+      <h1>Gerador de senhas</h1>
+      <label htmlFor="passwordSize">Tamanho: </label>
+      <NumberInput
+        passwordSize={passwordSize}
+        setPasswordSize={setPasswordSize}
+      />
+      <button onClick={generate}>
+        Gerar senha de {passwordSize} caracteres
+      </button>
+      <button onClick={copyToClipboard}>{copy}</button>
+      <div>{password}</div>
+    </div>
+  );
 }
-  
